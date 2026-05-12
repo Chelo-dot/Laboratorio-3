d25052b673851e2fb4ac9f05224378836e98e170
Chelo <marvillo028@gmail.com>
2026-05-11 19:50:05 -0600

    CREACION DEL EJ2

---
+++ Ej2/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SIZE 4
+
+int contar_unos_fila(int m[SIZE][SIZE], int fila);
+int contar_unos_columna(int m[SIZE][SIZE], int col);
+int es_identidad(int m[SIZE][SIZE]);
+int tiene_fila_completa(int m[SIZE][SIZE]);
+int tiene_columna_completa(int m[SIZE][SIZE]);
+
+int main(voit) {
+    int m[SIZE][SIZE];
+
+    return 0;
+}
