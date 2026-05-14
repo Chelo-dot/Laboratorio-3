b79cb37030b58d9e4d92c574cf30a2a6b72229e1
Chelo <marvillo028@gmail.com>
2026-05-14 12:03:21 -0600

    IMPLEMENTACION DE CONTAR_UNOS_FILA

--- Ej2/main.c
+++ Ej2/main.c
@@ -9,8 +9,25 @@ int es_identidad(int m[SIZE][SIZE]);
 int tiene_fila_completa(int m[SIZE][SIZE]);
 int tiene_columna_completa(int m[SIZE][SIZE]);
 
-int main(voit) {
-    int m[SIZE][SIZE];
+int main(void) {
+    int m[SIZE][SIZE] = {
+        {1, 0, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 0, 1}
+    };
+
+    printf("%d\n",contar_unos_fila(m, 2));
 
     return 0;
+}
+
+
+int contar_unos_fila(int m[SIZE][SIZE], int fila){
+    int contador = 0;
+    for(int i = 1; i < SIZE; i++){
+        if(m[fila-1][i] == 1) contador ++;
+    }
+
+    return contador;
 }
