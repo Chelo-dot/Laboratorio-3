1755f750988ac5e709a00203b0f6bc20a0503ef8
Chelo <marvillo028@gmail.com>
2026-05-16 20:25:17 -0600

    IMPLEMENTACION DE LECTURA Y CLASIFICACIONS Además, se corrigieron errores de syntax

--- Ej3/main.c
+++ Ej3/main.c
@@ -1,19 +1,45 @@
 #include <stdio.h>
 #define MAX_SENSORES 10
 
-typdef enum { NORMAL, ALERTA, FALLO } EstadosSensor;
+typedef enum { NORMAL, ALERTA, FALLO } EstadosSensor;
 
-typdef struc {
+typedef struct {
     int id;
     float valor;
-    EstadoSensor estado;
+    EstadosSensor estado;
 } Lectura;
 
-EstadoSensor clasificar(float valor);
-int contar_estado(Lectura s[], int n, Estado Sensor e);
+EstadosSensor clasificar(float valor);
+int contar_estado(Lectura s[], int n, EstadosSensor e);
 int lectura_extrema(Lectura s[], int n);
 
 int main(void){
 
+
+    // Lectura de sensores
+    Lectura s[MAX_SENSORES];
+    int n;
+    printf("Ingrese el número de sensores a ingresar (MAX 10):\n");
+    scanf("%d", &n);
+    int id = 0;
+    int valor = 0;
+    for(int i = 0; i < n; i++){
+        printf("Sensor %d \nid:", i);
+        scanf("%d", &id);
+        s[i].id = id;
+        printf("valor: ");
+        scanf("%d", valor);
+        s[i].valor = valor;
+        s[i].estado = clasificar(valor);
+    }
+
+    
+
     return 0;
+}
+
+EstadosSensor clasificar(float valor){
+    if(valor >= 40 && valor <= 60) return NORMAL;
+    else if(valor < 20 || valor > 80) return FALLO;
+    else return ALERTA;
 }
